c65869b6ea9103e2cc441f792dece3effa2be708
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2024-10-09 21:42:59 -0300

    criei uma expressão lambda que retorna true para números maiores que 10

--- LINQMotivacao1/LINQMotivacao1/Program.cs
+++ LINQMotivacao1/LINQMotivacao1/Program.cs
@@ -1,1 +1,14 @@
 ﻿
+
+using LINQMotivacao1;
+
+Console.WriteLine("## LINQ - Filtrar dados ##\n");
+
+// numeros = 1, 2, 4, 8, 32, 64, 128, 256, 512
+var numeros = FonteDados.GetNumeros();
+
+var resultado1 = numeros.Where(n => n < 10);
+
+Console.WriteLine(string.Join(" ", resultado1));
+
+Console.ReadKey();
