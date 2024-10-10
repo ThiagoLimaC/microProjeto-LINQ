e7954aea936e5864f659f5d9711d8b96906c5349
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2024-10-09 21:38:56 -0300

    Defini o método GetNumeros

--- LINQMotivacao1/LINQMotivacao1/FonteDados.cs
+++ LINQMotivacao1/LINQMotivacao1/FonteDados.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace LINQMotivacao1
 {
-    internal class FonteDados
+    public class FonteDados
     {
+        public static List<int> GetNumeros()
+        {
+            List<int> numeros = new List<int>()
+            {
+                1, 2, 4, 8, 16, 32, 64, 128, 256, 512
+            };
+            return numeros;
+        }
     }
 }
