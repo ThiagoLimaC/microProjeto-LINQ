7336455a558dd53d8db4d7ca4a783d8fb1b7058c
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2024-10-09 21:35:55 -0300

    removi todo o código antigo

--- LINQMotivacao1/LINQMotivacao1/Program.cs
+++ LINQMotivacao1/LINQMotivacao1/Program.cs
@@ -1,17 +1,1 @@
-﻿Console.WriteLine("## LINQ ##\n");
-
-IList<string> frutas = new List<string>() { "Banana", "Maça", "Pera", "Laranja", "Uva"};
-
-// query syntax
-var resultado = from f in frutas
-                where f.Contains('r')
-                select f;
-
-Console.WriteLine(String.Join(" ", resultado));
-
-// method syntax
-var resultado2 = frutas.Where(f => f.Contains('r'));
-
-Console.WriteLine(String.Join(" ", resultado2));
-
-Console.ReadKey();
+﻿
